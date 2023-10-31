cc86d783addd6aac80c4850239621474114bfd53
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-10-31 00:12:26 -0400

    Day 3, Day3 --v1--Logic--Main

--- Pruebas tecnicasx/src/com/PruebasTecnic/Rafael/Days/Main/Main.java
+++ Pruebas tecnicasx/src/com/PruebasTecnic/Rafael/Days/Main/Main.java
@@ -3,12 +3,13 @@ package com.PruebasTecnic.Rafael.Days.Main;
 
 import com.PruebasTecnic.Rafael.Days.day1.Day1;
 import com.PruebasTecnic.Rafael.Days.day2.Day2;
+import com.PruebasTecnic.Rafael.Days.day3.Day3;
 
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Prueba tecnica dia 2");
-        Day2 day2 = new Day2();
-        day2.bucleSumImp();
+        Day3 day3 = new Day3();
+        day3.numMaxBucle();
 
 
 
