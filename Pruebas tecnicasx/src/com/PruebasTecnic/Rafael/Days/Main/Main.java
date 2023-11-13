ceaa5f0b5524b368e5fbec5a64fae05b59f5d64e
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-11-13 00:37:36 -0400

    Day 7, Day7 --v1--Logic Main --firts

--- Pruebas tecnicasx/src/com/PruebasTecnic/Rafael/Days/Main/Main.java
+++ Pruebas tecnicasx/src/com/PruebasTecnic/Rafael/Days/Main/Main.java
@@ -7,12 +7,13 @@ import com.PruebasTecnic.Rafael.Days.day3.Day3;
 import com.PruebasTecnic.Rafael.Days.day4.Day4;
 import com.PruebasTecnic.Rafael.Days.day5.Day5;
 import com.PruebasTecnic.Rafael.Days.day6.Day6;
+import com.PruebasTecnic.Rafael.Days.day7.Day7;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Prueba tecnica dia 6 \n");
-        Day6 day6 = new Day6();
-        day6.buclefibonacci();
+        System.out.printf("Prueba tecnica dia 7 \n");
+        Day7 day7 = new Day7();
+        day7.numPrimos();
 
 
 
